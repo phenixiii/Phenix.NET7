8e60195816d974aaca4054c880ffba67e5a5d970
phenixiii <phenixiii@126.com>
2019-08-07 19:42:36 +0800

    Revert "20190807"
    This reverts commit 0ecfde2f84bcc392c4cca9fa7bef197ef85f53bc.

--- Phenix.Extensions/Phenix.WebApplication/Program.cs
+++ Phenix.Extensions/Phenix.WebApplication/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.IO;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 namespace Phenix.WebApplication
 {
@@ -15,25 +12,6 @@ namespace Phenix.WebApplication
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseStartup<Startup>()
-                /*
-                 * 使用轻量级跨平台服务 KestrelServer
-                 * 请根据自己系统的运行要求配置 KestrelServer 选项
-                 */
-                .UseKestrel(options =>
-                {
-                    options.Limits.KeepAliveTimeout = TimeSpan.FromMinutes(2); //保持活动状态超时
-                    options.Limits.MaxConcurrentConnections = 1000; //客户端最大连接数
-                    options.Limits.MaxConcurrentUpgradedConnections = 1000; //客户端最大连接数（其他协议如Websocket）
-                    options.Limits.MaxRequestBodySize = Int64.MaxValue; //请求正文最大大小
-                    options.Limits.MinRequestBodyDataRate =
-                        new MinDataRate(bytesPerSecond: 240, gracePeriod: TimeSpan.FromSeconds(5)); //请求正文最小数据速率
-                    options.Limits.MinResponseDataRate =
-                        new MinDataRate(bytesPerSecond: 240, gracePeriod: TimeSpan.FromSeconds(5)); //响应正文最小数据速率
-                    options.Limits.RequestHeadersTimeout = TimeSpan.FromMinutes(30); //请求标头超时
-                    options.AllowSynchronousIO = true; //是否允许对请求和响应使用同步 IO
-                })
-                .UseUrls("http://*:5000");
+                .UseStartup<Startup>();
     }
 }
