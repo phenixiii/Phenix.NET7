6842647dedad31abc08b9d989786f72de8f38b5f
phenixiii <phenixiii@126.com>
2020-02-22 13:52:33 +0800

    20200220 1，修订《Phenix.Services.Host.部署方法.doc》，补充'开发环境的搭建'等章节 2，补充和修订Demo.InspectionStation和Demo.InventoryControl的设计文档 3，提供《Phenix.Services.Host.分布式服务插件.pptx》，讲解WebAPI（应用）服务和Orleans（领域）服务的设计和开发方法

--- Demo_Practice/Demo.InspectionStation/Demo.InspectionStation.Plugin/Api/NetConfig.cs
+++ Demo_Practice/Demo.InspectionStation/Demo.InspectionStation.Plugin/Api/NetConfig.cs
@@ -8,21 +8,21 @@
         /// <summary>
         /// /api/center/listen
         /// </summary>
-        public const string ApiCenterListenPath = "/api/center/listen";
+        public const string ApiCenterListenPath = "/api/inspection-station/center/listen";
 
         /// <summary>
         /// /api/operation-point/weighbridge
         /// </summary>
-        public const string ApiOperationPointWeighbridgePath = "/api/operation-point/weighbridge";
+        public const string ApiOperationPointWeighbridgePath = "/api/inspection-station/operation-point/weighbridge";
 
         /// <summary>
         /// /api/operation-point/license-plate
         /// </summary>
-        public const string ApiOperationPointLicensePlatePath = "/api/operation-point/license-plate";
+        public const string ApiOperationPointLicensePlatePath = "/api/inspection-station/operation-point/license-plate";
 
         /// <summary>
         /// /api/operation-point/gate
         /// </summary>
-        public const string ApiOperationPointGatePath = "/api/operation-point/gate";
+        public const string ApiOperationPointGatePath = "/api/inspection-station/operation-point/gate";
     }
 }
